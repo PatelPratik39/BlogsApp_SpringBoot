dc068b44cb75a8e35f81d8b6e854e4e463356c97
PatelPratik39 <ptl.pratik717@gmail.com>
2024-06-09 13:55:53 -0500

    Basic APp

---
+++ Blogs-spring-boot/src/main/java/com/blogsApp/Blogs_spring_boot/BlogsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.blogsApp.Blogs_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogsSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogsSpringBootApplication.class, args);
+	}
+
+}
---
+++ Blogs-spring-boot/src/test/java/com/blogsApp/Blogs_spring_boot/BlogsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogsApp.Blogs_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogsSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
